4e099295b552122dc1129c4f58c87d4987cdc1f4
matheusgomes28 <matheusgarcia28@gmail.com>
2024-02-25 13:44:25 +0000

    Read toml config from admin app (#35)
    Read the toml config from the admin app too

--- cmd/urchin-admin/main.go
+++ cmd/urchin-admin/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 
@@ -12,15 +13,30 @@ import (
 )
 
 func main() {
-
 	// sets zerolog as the main logger
 	// in this APP
 	common.SetupLogger()
 
-	app_settings, err := common.LoadSettings()
-	if err != nil {
-		log.Fatal().Msgf("could not load app settings: %v", err)
-		os.Exit(-1)
+	config_toml := flag.String("config", "", "path to config toml file")
+	flag.Parse()
+
+	var app_settings common.AppSettings
+	if *config_toml != "" {
+		log.Info().Msgf("reading config file %s", *config_toml)
+		settings, err := common.ReadConfigToml(*config_toml)
+		if err != nil {
+			log.Error().Msgf("could not read toml: %v", err)
+			os.Exit(-2)
+		}
+		app_settings = settings
+	} else {
+		log.Info().Msgf("no config passed, reading environment variable settings")
+		settings, err := common.LoadSettings()
+		if err != nil {
+			log.Fatal().Msgf("could not load app settings: %v", err)
+			os.Exit(-1)
+		}
+		app_settings = settings
 	}
 
 	database, err := database.MakeSqlConnection(
