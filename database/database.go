52577341435f5e8549c435c63665af1a22d6e88f
Milad Rasouli <milad75rasouli@gmail.com>
2024-03-16 13:30:15 +0330

    check database connection

--- database/database.go
+++ database/database.go
@@ -174,11 +174,15 @@ func (db SqlDatabase) AddImage(uuid string, name string, alt string) (err error)
 }
 
 func MakeSqlConnection(user string, password string, address string, port int, database string) (SqlDatabase, error) {
-	/// Checking the DB connection
+
 	/// TODO : let user specify the DB
 	connection_str := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s", user, password, address, port, database)
 	db, err := sql.Open("mysql", connection_str)
 	if err != nil {
+		return SqlDatabase{}, err
+	}
+
+	if err := db.Ping(); err != nil {
 		return SqlDatabase{}, err
 	}
 	// See "Important settings" section.
