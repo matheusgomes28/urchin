5656fc024ee15a3629840c2ea13cc20c27ef42c1
matheusgomes28 <matheusgarcia28@gmail.com>
2024-03-29 12:26:14 +0000

    Remove test files from coverage data (#68)
    attempt to remove the reported coverage on test files by simply removing
    it from the coverage data.

--- tests/app_tests/endpoint_tests/posts_test.go
+++ tests/app_tests/endpoint_tests/posts_test.go
@@ -1,6 +1,7 @@
 package endpoint_tests
 
 import (
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -79,19 +80,22 @@ func TestPostFailurePostDoesntExist(t *testing.T) {
 		WebserverPort:    8080,
 	}
 
-	database_mock := mocks.DatabaseMock{}
+	database_mock := mocks.DatabaseMock{
+		GetPostHandler: func(int) (common.Post, error) {
+			return common.Post{}, fmt.Errorf("post doesn't exist")
+		},
+	}
 
 	router := app.SetupRoutes(app_settings, database_mock)
 	responseRecorder := httptest.NewRecorder()
 
-	request, err := http.NewRequest("GET", "/post/-1", nil)
+	request, err := http.NewRequest("GET", "/post/10000", nil)
 
 	require.Nil(t, err)
 
 	router.ServeHTTP(responseRecorder, request)
 
 	require.Equal(t, http.StatusInternalServerError, responseRecorder.Code)
-
 }
 
 func TestPostFailureNegativeInvalidKey(t *testing.T) {
@@ -105,17 +109,20 @@ func TestPostFailureNegativeInvalidKey(t *testing.T) {
 		WebserverPort:    8080,
 	}
 
-	database_mock := mocks.DatabaseMock{}
+	database_mock := mocks.DatabaseMock{
+		GetPostHandler: func(int) (common.Post, error) {
+			return common.Post{}, fmt.Errorf("post doesn't exist")
+		},
+	}
 
 	router := app.SetupRoutes(app_settings, database_mock)
 	responseRecorder := httptest.NewRecorder()
 
-	request, err := http.NewRequest("GET", "/post/10000", nil)
+	request, err := http.NewRequest("GET", "/post/-1", nil)
 
 	require.Nil(t, err)
 
 	router.ServeHTTP(responseRecorder, request)
 
 	require.Equal(t, http.StatusInternalServerError, responseRecorder.Code)
-
 }
