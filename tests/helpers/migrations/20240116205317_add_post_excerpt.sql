ae662efa81bab4be29b39592a122a0114a84d7f2
SourabHere <sourabbeargrylls@gmail.com>
2024-03-30 17:01:40 +0530

    removed helpers/migrations

--- tests/helpers/migrations/20240116205317_add_post_excerpt.sql
+++
@@ -1,9 +0,0 @@
--- +goose Up
--- +goose StatementBegin
-ALTER TABLE posts ADD COLUMN excerpt TEXT NOT NULL;
--- +goose StatementEnd
-
--- +goose Down
--- +goose StatementBegin
-ALTER TABLE posts DROP COLUMN excerpt;
--- +goose StatementEnd
--- tests/helpers/migrations/20240323203547_add_image_ext.sql
+++
@@ -1,9 +0,0 @@
--- +goose Up
--- +goose StatementBegin
-ALTER TABLE images ADD COLUMN ext TEXT NOT NULL;
--- +goose StatementEnd
-
--- +goose Down
--- +goose StatementBegin
-ALTER TABLE images DROP COLUMN ext;
--- +goose StatementEnd
