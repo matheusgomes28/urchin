b56ea393c46a4184e27f32235bbaee144d0d79c1
matheusgomes28 <matheusgarcia28@gmail.com>
2024-04-02 22:32:05 +0100

    Removing unnecessary code for env configuration

--- cmd/urchin-admin/main.go
+++ cmd/urchin-admin/main.go
@@ -22,22 +22,17 @@ func main() {
 
 	var app_settings common.AppSettings
 	if *config_toml != "" {
-		log.Info().Msgf("reading config file %s", *config_toml)
-		settings, err := common.ReadConfigToml(*config_toml)
-		if err != nil {
-			log.Error().Msgf("could not read toml: %v", err)
-			os.Exit(-2)
-		}
-		app_settings = settings
-	} else {
-		log.Info().Msgf("no config passed, reading environment variable settings")
-		settings, err := common.LoadSettings()
-		if err != nil {
-			log.Fatal().Msgf("could not load app settings: %v", err)
-			os.Exit(-1)
-		}
-		app_settings = settings
+		log.Error().Msgf("config not specified (--config)")
+		os.Exit(-1)
 	}
+
+	log.Info().Msgf("reading config file %s", *config_toml)
+	settings, err := common.ReadConfigToml(*config_toml)
+	if err != nil {
+		log.Error().Msgf("could not read toml: %v", err)
+		os.Exit(-2)
+	}
+	app_settings = settings
 
 	database, err := database.MakeSqlConnection(
 		app_settings.DatabaseUser,
