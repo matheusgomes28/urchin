57ffeb8b41c7fe005c05da054096ab592e85dfc1
matheusgomes28 <matheusgarcia28@gmail.com>
2024-05-29 22:11:57 +0100

    Adding support for recaptcha for contact pages

--- app/contact.go
+++ app/contact.go
@@ -2,8 +2,12 @@ package app
 
 import (
 	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
 	"net/http"
 	"net/mail"
+	"net/url"
 
 	"github.com/gin-gonic/gin"
 	"github.com/matheusgomes28/urchin/common"
@@ -12,7 +16,53 @@ import (
 	"github.com/rs/zerolog/log"
 )
 
-func makeContactFormHandler() func(*gin.Context) {
+const RECAPTCHA_VERIFY_URL string = "https://www.google.com/recaptcha/api/siteverify"
+
+type RecaptchaResponse struct {
+	Success   bool    `json:"success"`
+	Score     float32 `json:"score"`
+	Timestamp string  `json:"challenge_ts"`
+	Hostname  string  `json:"hostname"`
+}
+
+/*
+  "success": true|false,
+  "challenge_ts": timestamp,  // timestamp of the challenge load (ISO format yyyy-MM-dd'T'HH:mm:ssZZ)
+  "hostname": string,         // the hostname of the site where the reCAPTCHA was solved
+  "error-codes": [...]        // optional
+*/
+
+func verifyRecaptcha(recaptcha_secret string, recaptcha_response string) error {
+	// Validate that the recaptcha response was actually
+	// not a bot by checking the success rate
+	recaptcha_response_data, err := http.PostForm(RECAPTCHA_VERIFY_URL, url.Values{
+		"secret":   {recaptcha_secret},
+		"response": {recaptcha_response},
+	})
+	if err != nil {
+		err_str := fmt.Sprintf("could not do recaptcha post request: %s", err)
+		return fmt.Errorf("%s: %s", err_str, err)
+	}
+	defer recaptcha_response_data.Body.Close()
+
+	if recaptcha_response_data.StatusCode != http.StatusOK {
+		return fmt.Errorf("invalid recaptcha response: %s", recaptcha_response_data.Status)
+	}
+	var recaptcha_answer RecaptchaResponse
+	recaptcha_response_data_buffer, _ := io.ReadAll(recaptcha_response_data.Body)
+	err = json.Unmarshal(recaptcha_response_data_buffer, &recaptcha_answer)
+	if err != nil {
+		return fmt.Errorf("could not parse recaptcha response: %s", err)
+	}
+
+	if !recaptcha_answer.Success || (recaptcha_answer.Score < 0.9) {
+		return fmt.Errorf("could not validate recaptcha")
+	}
+
+	return nil
+}
+
+func makeContactFormHandler(app_settings common.AppSettings) func(*gin.Context) {
 	return func(c *gin.Context) {
 		if err := c.Request.ParseForm(); err != nil {
 			log.Error().Msgf("could not parse form %v", err)
@@ -25,6 +75,19 @@ func makeContactFormHandler() func(*gin.Context) {
 		email := c.Request.FormValue("email")
 		name := c.Request.FormValue("name")
 		message := c.Request.FormValue("message")
+		recaptcha_response := c.Request.FormValue("g-recaptcha-response")
+
+		// Make the request to Google's API
+		if (len(app_settings.RecaptchaSecret) > 0) && (len(app_settings.RecaptchaSiteKey) > 0) {
+			err := verifyRecaptcha(app_settings.RecaptchaSecret, recaptcha_response)
+			if err != nil {
+				log.Error().Msgf("could not validate recaptcha %v", err)
+				if err = render(c, http.StatusOK, views.MakeContactFailure("could not validate recaptcha", err.Error())); err != nil {
+					log.Error().Msgf("could not render %v", err)
+				}
+				return
+			}
+		}
 
 		// Parse email
 		_, err := mail.ParseAddress(email)
@@ -59,7 +122,7 @@ func makeContactFormHandler() func(*gin.Context) {
 
 // TODO : This is a duplicate of the index handler... abstract
 func contactHandler(c *gin.Context, app_settings common.AppSettings, db database.Database) ([]byte, error) {
-	index_view := views.MakeContactPage(app_settings.AppNavbar.Links)
+	index_view := views.MakeContactPage(app_settings.AppNavbar.Links, app_settings.RecaptchaSiteKey)
 	html_buffer := bytes.NewBuffer(nil)
 	if err := index_view.Render(c, html_buffer); err != nil {
 		log.Error().Msgf("could not render: %v", err)
