5a6baed1209c270b54a0aa7f5ecd4bc343978d3a
matheusgomes28 <matheusgarcia28@gmail.com>
2025-03-02 17:34:03 +0000

    unfinished work: endpoint for getting all cards

--- admin-app/app.go
+++ admin-app/app.go
@@ -23,6 +23,7 @@ func SetupRoutes(app_settings common.AppSettings, database database.Database) *g
 
 	r.POST("/pages", postPageHandler(database))
 
+	r.GET("/cards/:schema", getCardHandler(database))
 	r.POST("/cards", postCardHandler(database))
 	r.POST("/card-schemas", postSchemaHandler(database))
 
