9871cfa2d2e8fb969beca0343bd8ffb0d3f6ebd9
Ricardo <ricardobenthem@gmail.com>
2025-06-29 23:47:29 +0200

    Allow minified images to be served in images handler (#118)
    resize images at upload time
    
    ---------
    
    Co-authored-by: matheusgomes28 <matheusgarcia28@gmail.com>

--- admin-app/image.go
+++ admin-app/image.go
@@ -2,15 +2,20 @@ package admin_app
 
 import (
 	"fmt"
+	"image"
+	"image/jpeg"
+	"image/png"
 	"mime/multipart"
 	"net/http"
 	"os"
+	"path"
 	"path/filepath"
 
 	"github.com/fossoreslp/go-uuid-v4"
 	"github.com/gin-gonic/gin"
 	"github.com/matheusgomes28/urchin/common"
 	"github.com/rs/zerolog/log"
+	"golang.org/x/image/draw"
 )
 
 var allowed_extensions = map[string]bool{
@@ -19,6 +24,98 @@ var allowed_extensions = map[string]bool{
 
 var allowed_content_types = map[string]bool{
 	"image/jpeg": true, "image/png": true, "image/gif": true,
+}
+
+// Calculates the best suitable resize box for an image with the given
+// `original_width`, `original_height`, such that the returned (width, height)
+// dimensions will always be at most `max_width`, `max_height`
+func calculateResizeBox(original_width, original_height, max_height, desired_height int) (int, int) {
+	width_ratio := float64(max_height) / float64(original_width)
+	new_height := int(float64(original_height) * width_ratio)
+	if new_height <= desired_height {
+		return max_height, new_height
+	}
+
+	height_ratio := float64(desired_height) / float64(original_height)
+	new_width := int(float64(original_width) * height_ratio)
+	return new_width, int(height_ratio)
+}
+
+func resizeImage(src_path, dst_path string, desired_width, desired_height int) error {
+	// Open the source file
+	file, err := os.Open(src_path)
+	if err != nil {
+		return fmt.Errorf("could not open source image: %v", err)
+	}
+	defer file.Close()
+
+	// Decode image
+	img, format, err := image.Decode(file)
+	if err != nil {
+		return fmt.Errorf("could not decode image: %v", err)
+	}
+
+	// Calculate new size
+	bounds := img.Bounds()
+	new_width, new_height := calculateResizeBox(bounds.Dx(), bounds.Dy(), desired_width, desired_height)
+	dst := image.NewRGBA(image.Rect(0, 0, new_width, new_height))
+
+	// Resize using golang.org/x/image/draw
+	draw.CatmullRom.Scale(dst, dst.Bounds(), img, bounds, draw.Over, nil)
+
+	// Create new file
+	out, err := os.Create(dst_path)
+	if err != nil {
+		return fmt.Errorf("could not create output file: %v", err)
+	}
+	defer out.Close()
+
+	// Save based on format
+	switch format {
+	case "jpeg", "jpg":
+		err = jpeg.Encode(out, dst, &jpeg.Options{Quality: 85})
+	case "png":
+		err = png.Encode(out, dst)
+	case "gif":
+		err = png.Encode(out, dst)
+		log.Warn().Msg("GIF detected: resized image saved as PNG format with .gif extension")
+	default:
+		return fmt.Errorf("unsupported image format: %s", format)
+	}
+
+	if err != nil {
+		return fmt.Errorf("could not encode resized image: %v", err)
+	}
+
+	return nil
+}
+
+func createMinifiedImages(image_path string) error {
+
+	image_ext := path.Ext(image_path)
+	if len(image_ext) == 0 {
+		return fmt.Errorf("invalid image path: %s", image_path)
+	}
+	image_name := image_path[0 : len(image_path)-len(image_ext)]
+
+	image_types := []struct {
+		FileName  string
+		MaxWidth  int
+		MaxHeight int
+	}{
+		{FileName: fmt.Sprintf("%s_small%s", image_name, image_ext), MaxWidth: 200, MaxHeight: 200},
+		{FileName: fmt.Sprintf("%s_medium%s", image_name, image_ext), MaxWidth: 400, MaxHeight: 400},
+		{FileName: fmt.Sprintf("%s_large%s", image_name, image_ext), MaxWidth: 600, MaxHeight: 600},
+	}
+
+	for _, img_type := range image_types {
+		err := resizeImage(image_path, img_type.FileName, img_type.MaxWidth, img_type.MaxHeight)
+		if err != nil {
+			return fmt.Errorf("could not resize image: %v", err)
+		}
+	}
+
+	return nil
 }
 
 // TODO : need these endpoints
@@ -75,7 +172,6 @@ func postImageHandler(app_settings common.AppSettings) func(*gin.Context) {
 			c.JSON(http.StatusBadRequest, common.ErrorRes("file extension is not supported", err))
 			return
 		}
-
 		filename := fmt.Sprintf("%s%s", uuid.String(), ext)
 		image_path := filepath.Join(app_settings.ImageDirectory, filename)
 		err = c.SaveUploadedFile(file, image_path)
@@ -84,6 +180,14 @@ func postImageHandler(app_settings common.AppSettings) func(*gin.Context) {
 			c.JSON(http.StatusInternalServerError, common.ErrorRes("failed to upload image", err))
 			return
 		}
+
+		// Save lower dimensions of the image if needed
+		log.Info().Msgf("creating minified images for %s", image_path)
+		err = createMinifiedImages(image_path)
+		if err != nil {
+			log.Error().Msgf("could not create minified images: %v", err)
+		}
+
 		// End saving to filesystem
 		c.JSON(http.StatusOK, ImageIdResponse{
 			Id: uuid.String(),
--- common/image.go
+++ common/image.go
@@ -1,12 +1,15 @@
 package common
 
 type Image struct {
-	Uuid     string   `json:"uuid"`
-	Name     string   `json:"name"`
-	Filename string   `json:"filename"`
-	Filepath string   `json:"filepath"`
-	Ext      string   `json:"extension"`
-	Excerpt  string   `json:"excerpt"`
-	Location Location `json:"location"`
-	Date     string   `json:"date"`
+	Uuid           string   `json:"uuid"`
+	Name           string   `json:"name"`
+	Filename       string   `json:"filename"`
+	FilenameSmall  string   `json:"filename_small"`
+	FilenameMedium string   `json:"filename_medium"`
+	FilenameLarge  string   `json:"filename_large"`
+	Filepath       string   `json:"filepath"`
+	Ext            string   `json:"extension"`
+	Excerpt        string   `json:"excerpt"`
+	Location       Location `json:"location"`
+	Date           string   `json:"date"`
 }
