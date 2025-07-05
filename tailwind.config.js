1a945f47bfc41c0866d17025c2622d154df4b508
AlDu2407 <40807522+AlDu2407@users.noreply.github.com>
2025-07-05 19:07:19 +0200

    feat: Theme (#130)
    - Added new theme to urchin.
    - Updated templates to use new theme colors.
    - Removed explicit 'dark' selector when specifying colors to prevent
    flickering when switching pages.
    
    ---------
    
    Co-authored-by: matheusgomes28 <matheusgarcia28@gmail.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,33 +1,37 @@
 /** @type {import('tailwindcss').Config} */
-export const content = [
-  "./views/*.templ",
-]
-
-export const theme = {
-  container: {
-    center: true,
-    padding: {
-      DEFAULT: "1rem",
-      mobile: "1rem",
-      tablet: "1rem",
-      desktop: "1rem",
+module.exports = {
+  content: ["./views/*.templ"],
+  darkMode: "class",
+  theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1rem",
+        mobile: "1rem",
+        tablet: "1rem",
+        desktop: "1rem",
+      },
+    },
+    extend: {
+      colors: {
+        "urchin-bg-dark": "var(--color-urchin-bg-dark)",
+        "urchin-bg": "var(--color-urchin-bg)",
+        "urchin-bg-light": "var(--color-urchin-bg-light)",
+        "urchin-text": "var(--color-urchin-text)",
+        "urchin-text-muted": "var(--color-urchin-text-muted)",
+        "urchin-highlight": "var(--color-urchin-highlight)",
+        "urchin-border": "var(--color-urchin-border)",
+        "urchin-border-muted": "var(--color-urchin-border-muted)",
+        "urchin-primary": "var(--color-urchin-primary)",
+        "urchin-primary-highlight": "var(--color-urchin-primary-highlight)",
+        "urchin-secondary": "var(--color-urchin-secondary)",
+        "urchin-secondary-highlight": "var(--color-urchin-secondary-highlight)",
+        "urchin-danger": "var(--color-urchin-danger)",
+        "urchin-warning": "var(--color-urchin-warning)",
+        "urchin-success": "var(--color-urchin-success)",
+        "urchin-info": "var(--color-urchin-info)",
+      },
     },
   },
-  extend: {
-    colors: {
-      pastel: {
-        blue: '#AEC6CF',
-        purple: '#CBAACB',
-        pink: '#FFB6C1',
-        orange: '#FFDAB9',
-        green: '#C4E17F',
-        yellow: '#FFDD94',
-        gray: '#E5E5E5'
-      }
-    }
-  }
-}
-
-export const darkMode = 'selector'
-
-export const plugins = [require("@tailwindcss/forms"), require("@tailwindcss/typography")]
+  plugins: [require("@tailwindcss/forms"), require("@tailwindcss/typography")],
+};
